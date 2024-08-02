b64e24bb4ccb8893607c753a61836db8361b5975
yousofsayed <yousef.sayed1231@gmail.com>
2024-08-01 17:03:49 -0700

    cm-6

--- src/components/Home/Iframe.jsx
+++ src/components/Home/Iframe.jsx
@@ -6,15 +6,7 @@ import {
   refsStt,
   showPopupIframState,
 } from "../../helpers/atoms";
-// import style from "/styles/style.css";
-
-import {
-  appendStyle,
-  cleaner,
-  iframeHandler,
-  removeOldScriptsAndStyles,
-} from "../../helpers/functions";
-import { html } from "../../helpers/cocktail";
+import { cleaner, iframeHandler } from "../../helpers/functions";
 
 let firstRender = 0;
 
@@ -23,24 +15,24 @@ export const Iframe = () => {
   const showPopUp = useRecoilValue(showPopupIframState);
   const setIframeRef = useSetRecoilState(refsStt);
   const setIframeBody = useSetRecoilState(iframeBody);
-  const setIframeWindow = useSetRecoilState(iframeWindow);
 
   useEffect(() => {
     firstRender++;
-    if (ifrRef.current && firstRender == 2) {
+    if (ifrRef.current) {
+      console.log(ifrRef.current);
       setIframeRef((oldstt) => ({ ...oldstt, ifrRef: ifrRef.current }));
       setIframeBody(ifrRef.current.contentDocument);
       iframeHandler(ifrRef.current);
-      const script = document.createElement('script');
-      script.src = '/scripts/main.js';
+      const script = ifrRef.current.contentDocument.createElement("script");
+      script.src = "/scripts/main.js";
       ifrRef.current.contentDocument.body.appendChild(script);
     }
 
     return cleaner;
-  }, [ifrRef]);
+  });
 
   return (
-    <section draggable={true} className="relative flex-grow h-full">
+    <section  className="relative flex-grow h-full">
       {showPopUp && (
         <div className="absolute w-full h-full bg-slate-400 z-10 opacity-[.5]"></div>
       )}
